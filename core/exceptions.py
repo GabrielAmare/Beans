10f66acd393bb98586af22685c07a7077316a507
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2020-12-10 18:48:25 +0100

    Update exceptions.py
    Added two exceptions for the PrimaryKey Constraint
    -> to check integrity of a bean config before creation
    -> to check intergity of a bean config before update

--- core/exceptions.py
+++ core/exceptions.py
@@ -40,3 +40,31 @@ class ValueNotInRangeError(FieldValueError):
 class ValueLengthError(FieldValueError):
     """Raised when the value length is not in the specified range of the field"""
     message = "{value!r} length should be in range [{constraint.min_value}: {constraint.max_value}]"
+
+
+class PrimaryKeyCreateError(Exception):
+    def __init__(self, bean_class, config):
+        self.bean_class = bean_class
+        self.config = config
+
+    def __repr__(self):
+        return "Can't create the {bean_class_name} instance, primary key error !\nconfig was {config}".format(
+            bean_class_name=self.bean_class.__name__,
+            config=self.config.__repr__()
+        )
+
+
+class PrimaryKeyUpdateError(Exception):
+    def __init__(self, bean_original, new_config, bean_conflict):
+        self.bean_original = bean_original
+        self.new_config = new_config
+        self.bean_conflict = bean_conflict
+
+    def __repr__(self):
+        return "Can't update a {name1}:{uid1} instance, conflict with {name2}:{uid2}, primary key error !\nwith update config = {config}".format(
+            name1=self.bean_original.__class__.__name__,
+            uid1=self.bean_original.uid.__repr__(),
+            name2=self.bean_original.__class__.__name__,
+            uid2=self.bean_original.uid.__repr__(),
+            config=self.new_config.__repr__()
+        )
